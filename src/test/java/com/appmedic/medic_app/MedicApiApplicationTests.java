aab37071dc22d9c368187a6fbd89186e95edf88d
John Cuvi :) <vladimircuvi@coopdaquilema.com>
2024-10-04 05:18:13 -0500

    Spring #1 Tarea-1: Feat: Init proyect

---
+++ src/test/java/com/appmedic/medic_app/MedicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appmedic.medic_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
